dc3d76477b387992743dcfcab563866224f91a8b
gus <angusgyoung@gmail.com>
2022-08-28 21:48:56 +0100

    Add search

---
+++ src/rightmove/search.ts
@@ -0,0 +1,27 @@
+import { rightMoveApi } from "./api"
+
+export interface SearchParams {
+    locationIdentifier: String,
+    maxBedrooms: Number,
+    minBedrooms: Number,
+    maxPrice: Number,
+    minPrice: Number,
+    numberOfPropertiesPerPage: Number,
+    radius: Number,
+    // Sort enum
+    sortType: 2,
+    index: Number,
+    propertyTypes: ["bungalow, detached", "flat", "semi-detached", "terraced"],
+    viewType: "LIST",
+    dontShow: ["houseShare", "student"],
+    channel: "RENT",
+    areaSizeUnit: "sqft",
+    currencyCode: "GBP",
+    isFetching: false,
+    viewport: ""
+}
+
+export const search = async (locationIdentifier: String, params: SearchParams) => {
+    rightMoveApi.get()
+}
+
