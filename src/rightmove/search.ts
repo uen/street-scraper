8043fe412e21a45f2c22e5a08aa4f18ee5c08563
gus <angusgyoung@gmail.com>
2022-08-28 21:58:23 +0100

    Search call

--- src/rightmove/search.ts
+++ src/rightmove/search.ts
@@ -1,9 +1,16 @@
 import { rightMoveApi } from "./api"
+import { IRightMoveProperty } from "./IRightMoveProperty"
 import { SearchParams } from "./ISearchParams"
 
-export const search = async (searchParams: SearchParams) => {
-    rightMoveApi.get('', {
+export const search = async (searchParams: SearchParams): Promise<IRightMoveProperty> => {
+    const { status, data } = await rightMoveApi.get<IRightMoveProperty>('api/_search', {
         params: searchParams
     })
+
+
+    if (status === 200 && data)
+        return data
+
+    throw new Error(`Failed to execute search: ${searchParams}`)
 }
 
