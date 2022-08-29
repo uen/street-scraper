d7e6f5ebbce5bc8debd2a8fd7bdbc68510452baf
gus <angusgyoung@gmail.com>
2022-08-29 02:27:07 +0100

    Fix markdown

--- src/util/text-generation.ts
+++ src/util/text-generation.ts
@@ -3,13 +3,13 @@ import { IProperty } from "../interface/IProperty";
 
 export const generateNewPropertyMessage = (newProperties: IProperty[]) => {
   return `
-*New Properties*
-** ${newProperties.length} new properties
+**New Properties**
+*${newProperties.length} new properties*
 ${newProperties
   .filter((property) => property.displayAddress)
   .map(
     (property, index) =>
-      `${index}. ${property.displayAddress} ${property.price.amount}`
+      `${index}. ${property.displayAddress} ${property.price.amount}\n${property.propertyUrl}`
   )
   .join("\n")}
     `;
@@ -19,13 +19,13 @@ export const generateReducedPropertyMessage = (
   reducedProperties: IPriceChangeProperty[]
 ) => {
   return `
-    *Reduced Properties*
-    ** ${reducedProperties.length} reduced properties
+    **Reduced Properties**
+    *${reducedProperties.length} reduced properties*
     ${reducedProperties
       .filter((reducedProperty) => reducedProperty.property.displayAddress)
       .map(
         (reducedProperty, index) =>
-          `${index}. ${reducedProperty.property.displayAddress} ${reducedProperty.property.price.amount} (${reducedProperty.percentageDifference})`
+          `${index}. ${reducedProperty.property.displayAddress} ${reducedProperty.property.price.amount} (${reducedProperty.percentageDifference})\n${reducedProperty.property.propertyUrl}`
       )
       .join("\n")}
         `;
