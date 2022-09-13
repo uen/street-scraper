a0072c07f1ac4c24a60f2d484a86bf59b2e5b02d
gus <angusgyoung@gmail.com>
2022-09-13 09:54:08 +0000

    Simplify regex

--- src/util/area.ts
+++ src/util/area.ts
@@ -1,9 +1,9 @@
 import { first } from "lodash";
 
-const POSTCODE_REGEX = /[a-zA-Z]{1,2}\d{1,2}(?:\s\d[a-zA-Z]{2})?/;
+const POSTCODE_REGEX = /\w{1,2}\d{1,2}(?:\s?\d\w{2})?/i;
 
 // https://regex101.com/r/eW6qEu/1
-const POSTCODE_INCLUDE_REGEX = /(?:N|SW|SE|E|W)\d{1,2}(?:\s\d[a-z]{2})?/i;
+const POSTCODE_INCLUDE_REGEX = /(?:N|SW|SE|E|W)\d{1,2}(?:\s?\d\w{2})?/i;
 const AREA_EXCLUDE_REGEX = /surrey|croydon|twickenham|kingston|camberley/i;
 
 export const parseArea = (
